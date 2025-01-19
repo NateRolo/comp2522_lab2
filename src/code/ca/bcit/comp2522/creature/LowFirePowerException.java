97b459b43cdec6da96f60293f166e93de023f84e
Nathan Oloresisimo <146795346+NateRolo@users.noreply.github.com>
2025-01-19 13:34:30 -0800

    Wrote LowFirePowerException class, extends Exception and gets throw if dragon attempts to breathe fire with less than required amount.

---
+++ src/code/ca/bcit/comp2522/creature/LowFirePowerException.java
@@ -0,0 +1,9 @@
+package ca.bcit.comp2522.creature;
+
+public class LowFirePowerException extends Exception
+{
+    LowFirePowerException(final String message)
+    {
+        super(message);
+    }
+}
