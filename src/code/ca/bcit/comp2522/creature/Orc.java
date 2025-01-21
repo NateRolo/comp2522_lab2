831316ab8ce04eda0953f7bcda6287277e11682f
Nathan Oloresisimo <146795346+NateRolo@users.noreply.github.com>
2025-01-21 13:31:46 -0800

    feat: Wrote constructor and validation method for rage.

--- src/code/ca/bcit/comp2522/creature/Orc.java
+++ src/code/ca/bcit/comp2522/creature/Orc.java
@@ -1,6 +1,49 @@
 package ca.bcit.comp2522.creature;
 
-//public class Orc extends Creature
-//{
-//
-//}
+public class Orc extends Creature
+{
+    private static final int MAXIMUM_RAGE;
+    private static final int MINIMUM_RAGE;
+
+    private int rage;
+
+    static
+    {
+        MAXIMUM_RAGE = 30;
+        MINIMUM_RAGE = 0;
+    }
+
+    public Orc(final String name,
+               final Date dateOfBirth,
+               final int health,
+               final int rage)
+    {
+        super(name,
+              dateOfBirth,
+              health);
+
+        validateRage(rage);
+        this.rage = rage;
+    }
+
+    private static void validateRage(final int rage)
+    {
+        final boolean rageExceedsMaximum;
+        final boolean rageLowerThanMinimum;
+
+        rageExceedsMaximum = rage > MAXIMUM_RAGE;
+        rageLowerThanMinimum = rage < MINIMUM_RAGE;
+
+        if(rageExceedsMaximum)
+        {
+            throw new IllegalArgumentException("Rage cannot exceed " +
+                                               MAXIMUM_RAGE);
+        }
+
+        if(rageLowerThanMinimum)
+        {
+            throw new IllegalArgumentException("Rage cannot be lower than" +
+                                               MINIMUM_RAGE);
+        }
+    }
+}
