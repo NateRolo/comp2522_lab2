682c5b912d1c2e0c424cfcb1e9b299fc03103d1d
Nathan Oloresisimo <146795346+NateRolo@users.noreply.github.com>
2025-01-21 18:46:56 -0800

    feat: created 3 Creature objects using polymorphism.

---
+++ src/tests/CreatureTest.java
@@ -0,0 +1,43 @@
+import ca.bcit.comp2522.creature.Elf;
+import ca.bcit.comp2522.creature.Orc;
+import ca.bcit.comp2522.creature.Dragon;
+import ca.bcit.comp2522.creature.Date;
+import ca.bcit.comp2522.creature.Creature;
+
+public class CreatureTest
+{
+    public static void main(final String[] args)
+    {
+        final Date dragonDateOfBirth;
+        final Date elfDateOfBirth;
+        final Date orcDateOfBirth;
+
+        final Creature testDragon;
+        final Creature testElf;
+        final Creature testOrc;
+
+        dragonDateOfBirth = new Date(2000,
+                                     1,
+                                     1);
+        elfDateOfBirth = new Date(1990,
+                                  6,
+                                  12);
+        orcDateOfBirth = new Date(1998,
+                                  9,
+                                  25);
+
+
+        testDragon = new Dragon("Spyro",
+                                dragonDateOfBirth,
+                                100,
+                                100);
+        testElf = new Elf("Dobby",
+                          elfDateOfBirth,
+                          80,
+                          49);
+        testOrc = new Orc("Azog",
+                          orcDateOfBirth,
+                          50,
+                          20);
+    }
+}
